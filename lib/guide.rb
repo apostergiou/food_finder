52cc38d1f0da03c71137e46a2c5fc46341b8d99c
Apostolis Stergiou <apostergiou@gmail.com>
2016-10-29 22:37:59 +0300

    Fix indentation

--- lib/guide.rb
+++ lib/guide.rb
@@ -29,8 +29,8 @@ class Guide
     puts "\n\n<<< Welcome to the Food Finder >>>\n\n"
     puts "This is an interactive guide to help you find the food you crave.\n\n"
   end
-
-	def conclusion
-  	puts "\n<<< Goodbye and Bon Appetit! >>>\n\n\n"
-	end
+	
+  def conclusion
+    puts "\n<<< Goodbye and Bon Appetit! >>>\n\n\n"
+  end
 end
